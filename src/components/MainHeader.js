0834a14e57cd31d462ffb896aead9898c2f7a358
Sebastian Bocaciu <75611075+TheAppleJuice@users.noreply.github.com>
2021-09-06 16:46:50 +0200

    Added NavLink and css

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,17 +1,26 @@
+import { NavLink } from "react-router-dom";
+
+import classes from "./MainHeader.module.css";
+
 const MainHeader = () => {
   return (
-    <header>
+    <header className={classes.header}>
       <nav>
         <ul>
           <li>
-            <a href="/welcome">Welcome</a>
+            <NavLink activeClassName={classes.active} to="/welcome">
+              Welcome
+            </NavLink>
           </li>
           <li>
-            <a href="/products">Products</a>
+            <NavLink activeClassName={classes.active} to="/products">
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>
     </header>
   );
 };
+
 export default MainHeader;
